a9b4baf82b5783690709a08b9663328a09741cdc
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-02-05 16:53:56 +0530

    Added two more menu items in the Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,24 +6,26 @@ import { removeUser } from "../utils/userSlice";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  const photo = user?.data?.photoUrl
+  const photo = user?.data?.photoUrl;
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const handleLogout = async () =>{
-    try{
-      await axios.post(BASE_URL + "/logout", {}, {withCredentials : true})
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
       dispatch(removeUser());
       navigate("/login");
-    }catch(err){
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
   return (
     <div className="navbar bg-base-300 shadow-sm">
-        <div className="flex-1">
-          <Link to={"/"} className="btn btn-ghost text-xl">DevTinder</Link>
-        </div>
-        {user?.data && (
+      <div className="flex-1">
+        <Link to={"/"} className="btn btn-ghost text-xl">
+          DevTinder
+        </Link>
+      </div>
+      {user?.data && (
         <div className="flex gap-2">
           <div className="dropdown dropdown-end mx-5">
             <div
@@ -32,10 +34,7 @@ const NavBar = () => {
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img
-                  alt="Photo"
-                  src={photo}
-                />
+                <img alt="Photo" src={photo} />
               </div>
             </div>
             <ul
@@ -45,11 +44,17 @@ const NavBar = () => {
               <li>
                 <Link to={"/profile"} className="justify-between">
                   Profile
-                  <span className="badge">New</span>
                 </Link>
               </li>
               <li>
-                <a>Settings</a>
+                <Link to={"/connections"} className="justify-between">
+                  My Connections
+                </Link>
+              </li>
+              <li>
+                <Link to={"/requests"} className="justify-between">
+                  Requests
+                </Link>
               </li>
               <li>
                 <button onClick={handleLogout}>Logout</button>
@@ -57,10 +62,9 @@ const NavBar = () => {
             </ul>
           </div>
         </div>
-        )}
+      )}
+    </div>
+  );
+};
 
-      </div>
-  )
-}
-
-export default NavBar
+export default NavBar;
