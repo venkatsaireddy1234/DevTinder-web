0944abc9c2ff7f786d715cc957ba01990d768ba4
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-02-05 16:53:39 +0530

    Added the Animation loader in the Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import UserCard from "./UserCard";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
@@ -7,13 +7,20 @@ import { addFeed } from "../utils/feedSlice";
 
 const Feed = () => {
   const dispatch = useDispatch();
-  const userFeed = useSelector((store)=> store.feed);
+  const userFeed = useSelector((store) => store.feed);
+  const currentUser = useSelector((store) => store.user);
+  console.log(currentUser)
+  const [isLoading, setIsLoading] = useState(true);
   const fetchFeed = async () => {
     try {
-      let userFeed = await axios.get(BASE_URL + "/user/feed", { withCredentials: true });
+      let userFeed = await axios.get(BASE_URL + "/user/feed", {
+        withCredentials: true,
+      });
       dispatch(addFeed(userFeed?.data?.allUsers[0]));
     } catch (err) {
       console.log(err.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -21,8 +28,33 @@ const Feed = () => {
     fetchFeed();
   }, []);
 
-  return userFeed && (
-  <UserCard user={userFeed}/> );
+  if (isLoading) {
+    const profilePhoto =
+      currentUser?.data?.photoUrl || "https://via.placeholder.com/120?text=You";
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="relative flex items-center justify-center">
+          <span className="absolute h-28 w-28 rounded-full bg-rose-400/30 animate-ping"></span>
+          <span className="absolute h-36 w-36 rounded-full border-2 border-rose-400/40"></span>
+          <div className="h-20 w-20 rounded-full overflow-hidden border-2 border-rose-500 shadow-md">
+            <img
+              src={profilePhoto}
+              alt="Your profile"
+              className="h-full w-full object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    userFeed && (
+      <div className="flex items-center justify-center m-10">
+        <UserCard user={userFeed} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
