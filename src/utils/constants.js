c1766c4c301310576830e9d2f6072afabd187b62
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-02-09 15:04:22 +0530

    Changed the Base_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL =
-  import.meta.env.VITE_BASE_URL || "http://localhost:7777";
+const rawBaseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:7777";
+export const BASE_URL = rawBaseUrl.replace(/\/+$/, "");
