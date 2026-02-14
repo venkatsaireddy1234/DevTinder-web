dee14e97d8b851b19d4b73fe08f0a35b778660dd
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2026-02-14 09:35:28 +0530

    Update the constants for the vercel deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
-// const rawBaseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:7777" || "/api";
-// export const BASE_URL = rawBaseUrl.replace(/\/+$/, "");
+const rawBaseUrl = import.meta.env.VITE_BASE_URL || "http://localhost:7777";
+export const BASE_URL = rawBaseUrl.replace(/\/+$/, "");
 
 
 
 // Local 
-export const BASE_URL = location.hostname === 'localhost'  ? "http://localhost:7777" : "/api"
+// export const BASE_URL = location.hostname === 'localhost'  ? "http://localhost:7777" : "/api"
